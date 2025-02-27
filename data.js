eed29cea8c2bb036919702f18a5c908b95b3a17f
rathnaa-chh <chhourn.rathana.2823@rupp.edu.kh>
2025-02-27 17:33:35 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,119 @@
+export const places = [
+    {
+        id: 1,
+        image: 'https://www.asiakingtravel.com/cuploads/files/koh-rong-island-1.jpg',
+        category: 'Island',
+        title: 'Koh Rong',
+        description: 'Located just off the coast of Sihanoukville, Koh Rong is one of the most famous islands in Cambodia. It is known for its serene beauty and breathtaking scenery. Described as an "island paradise" by visiting tourists, it is a perfect holiday destination for families, couples, and small groups of travellers.',
+        views: 340,
+
+    },
+    {
+        id: 2,
+        image: 'https://q-xx.bstatic.com/xdata/images/hotel/max500/499609828.jpg?k=ff7ffc2c1866a667a6f05252e101eaa1afb83d919a845d4528070dfc5c19f0ee&o=',
+        category: 'Island',
+        title: 'Koh Sdach',
+        description: 'Koh Sdach is the capital island of the Koh Sdach archipelago. The bean-shaped island stretches from North to South on a length of around 2.8 km (2 mi), it is rather flat and generally forested. Natural jungle has been replaced by coconut trees and forest crops for commercial purposes.',
+        views: 1000,
+
+    },
+    {
+        id: 3,
+        image: 'https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTGwVa2x0c4RVl-4wQIqXYT9a9uu3REVmnF1Wy5Uhjfwayh_q-MyqW0gwBG299zRw-crdW8yGT4gs_LqsZJW0mvNXMuggLEnSGoMYPp8Q',
+        category: 'Temple',
+        title: 'Angkor Wat',
+        description: 'Angkor Wat, Temple complex in Angkor (now in northwestern Cambodia), the crowning work of Khmer architecture. About 1,700 yards (1,550 m) long by 1,500 yards (1,400 m) wide, it is the world’s largest religious structure. Dedicated to Vishnu, it was built in the 12th century by Suryavarman II. The Wat, an artificial mountain originally surrounded by a vast external wall and moat, rises in three enclosures toward a flat summit. The five remaining towers (shrines) at the summit are composed of the repetitive diminishing tiers typical of Asian architecture.',
+        views: 2000,
+
+    },
+    {
+        id: 4,
+        image: 'https://lh3.googleusercontent.com/p/AF1QipOaK8vIgiygphoe6ZzL6FvDbiRU1i1vb10ji5ER=s680-w680-h510',
+        category: 'Temple',
+        title: 'Ta Prohm',
+        description: 'The so-called "Tomb Raider Temple", Ta Prohm is cloaked in dappled shadow, its crumbling towers and walls locked in the slow muscular embrace of vast root systems. Undoubtedly the most atmospheric ruin at Angkor, Ta Prohm should be high on the hit list of every visitor. Its appeal lies in the fact that, unlike the other monuments of Angkor, it has been swallowed by the jungle, and looks very much the way most of the monuments of Angkor appeared when European explorers first stumbled upon them.',
+        views: 3100,
+
+    },
+    {
+        id: 5,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxwDHcgeEPaV713w48w-JrBmnIA8DSlbwmPA&s',
+        category: 'Temple',
+        title: 'Koh Ker',
+        description: 'Koh Ker Temple in Cambodia is a huge temple complex that served as the capital of the Khmer empire for a brief period between 928AD and 944AD. It is located 118kilometers northeast of Siem Reap and as it is the case with other remote temples, Koh Ker has not been restored and is one of the least studied temple-complex.',
+        views: 1920,
+
+    },
+    {
+        id: 6,
+        image: 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/183945591.jpg?k=bd2e21d35f77068321f140aa46683d8391be4d22359de38d5dd053357be31b1c&o=&hp=1',
+        category: 'Island',
+        title: 'Koh Song Saa',
+        description: 'Song Saa Private Island is a heaven of sustainable luxury nestled in the undiscovered beauty of Cambodia is Koh Rong Archipelago. With unique cuisine and stunning sunset views of the Gulf of Thailand, and an array of spa and wellness treatments, Song Saa offers the ultimate indulgence for mind, body and soul.',
+        views: 7200,
+
+    },
+    // {
+    //     id:7,
+    //     image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5NZRbWyym4-JQrd5DduNGEuGuUgXK4b3RLQ&s',
+    //     category:'Temple',
+    //     title:'Bayon',
+    //     description:'...............',
+    //     views:2800,
+
+    // },
+    {
+        id: 8,
+        image: 'https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcQXgnLxBuHZG76O7BVpfOd8UI82ShjYNmewZZ7p8Flcz1_nvA1kJHqB75-z24WYTazRjUOH31bbAZZi7mK9-X9kACRt7eGuUsKIOUxgLA',
+        category: 'Temple',
+        title: 'Praeh Khan',
+        description: 'Rectangular in shape and occupying 138 acres, Preah Khan"s boundaries are defined by a protective moat and fortified walls adorned by monumental carved stone garudas (eagle-like divine beings). The temple complex includes entryways, towers, ceremonial spaces, courtyards, shrines, and a variety of connecting corridors',
+        views: 6000,
+
+    },
+    {
+        id: 9,
+        image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/03/9e/f9/a2/koh-tonsay-rabbit-island.jpg?w=1200&h=-1&s=1',
+        category: 'Island',
+        title: 'Koh Tonsay(Rabbit Island)',
+        description: 'Koh Tonsay is located just 4 km (2 mi) south of Kep town, has an area of around 2 km2 (1 sq mi) and can be reached by ferry boat from the local port. The island is open for visitors and tourists, who value white sand beaches and the marine scenery.',
+        views: 4545,
+
+    },
+    {
+        id: 10,
+        image: 'https://cdn.getyourguide.com/img/tour/e6926dbd12c66d791084b6125a6b30b3119d69db9600f94a696c8988b8546c51.jpg/146.jpg',
+        category: 'Mountain',
+        title: 'Phnom Bokor',
+        description: 'The Bokor Hill is 1,075 meters high, and promises good weather and magnificent views. Big trees casting shadows over the water and rocks shaped as animal figures hold particular interest for tourists. The mountain overlooks Kampot Town, Kep Beach, Preah Sihanouk Province and blue sea water.',
+        views: 4700,
+
+    },
+    {
+        id: 11,
+        image: 'https://www.khmerplaces.com/storage/posts/May2020/TWr5kUEIatKGO7ACMRu9.jpg',
+        category: 'Mountain',
+        title: 'Phnom Khnang Phsa',
+        description: 'Khnang Phsa Mountain is a large mountainous area with three intersections connecting three provinces: Kampong Speu, Koh Kong and Pursat. It is an area of ​​dense forest, high valleys, beautiful pines, and many species of rare wildlife. It is about 1017 meters high with 360 degree views which great for those who like hiking. Khnang Phsa Mountain is beautiful all seasons is about 165km from Phnom Penh.',
+        views: 4731,
+
+    },
+    {
+        id: 12,
+        image: 'https://www.guidingcambodia.com/wp-content/uploads/2023/12/Phnom-Kulen-Waterfall-02-853x640-1.jpg',
+        category: 'Mountain',
+        title: 'Phnom Kulen',
+        description: 'Its name means "mountain of the lychees". There is a sacred hilltop site on top of the range. Phnom Kulen is considered a holy mountain in Cambodia, of special religious significance to Hindus and Buddhists who come to the mountain in pilgrimage.',
+        views: 4702,
+
+    },
+    {
+        id: 13,
+        image: 'https://www.asiakingtravel.com/cuploads/files/Phnom-Aural-mountain-4.jpg',
+        category: 'Mountain',
+        title: 'Phnom Aural',
+        description: 'Phnom Aural (Khmer: ភ្នំឱរ៉ាល់ [pʰnom ʔaoral]), also spelled Phnom Aoral, is the tallest peak in Cambodia. It is 1,813 meters tall (other sources give elevations between 1,771 and 1,667 meters). It is in the eastern part of the Cardamom Mountains.',
+        views: 4007,
+
+    },
+];
